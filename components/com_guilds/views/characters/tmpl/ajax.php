17a4f16050c7ced76803354355e5386bf9451e42
Nick Swinford <NicholasJohn16@gmail.com>
2012-06-27 03:40:16 -0400

    removed div padding elements
    just use select elements for the table

--- components/com_guilds/views/characters/tmpl/ajax.php
+++ components/com_guilds/views/characters/tmpl/ajax.php
@@ -1,38 +1,36 @@
 <?php if(count($this->characters) == 0): ?>
-	<div class="alert alert-block guild-container" style="margin-bottom:0;float:none;">
+	<div class="alert alert-block com-guilds-container" style="margin-bottom:0;float:none;">
 		<a class="close" data-dismiss="alert" href="#">&times;</a>
 		<strong>Uh oh!</strong>  This user doesn't have any characters yet.  Click the <strong>Add Character</strong> button below to add one!
 	</div>
 <?php else:; ?>
-<div class="guild-container">
-	<div class="guild-header">
+<div class="com-guilds-container">
+	<div class="com-guilds-header">
 		<div style="width:4%;">ID</div>
 		<div style="width:2%"><input type="checkbox" class="checkall" title="Check All"/></div>
 		<div style="width:10%;">Name</div>
 		<?php foreach($this->types as $type):?>
-			<div class="guild-<?php echo $type->name; ?>"><?php echo ucwords($type->name); ?></div>
+			<div class="com-guilds-<?php echo $type->name; ?>"><?php echo ucwords($type->name); ?></div>
 		<?php endforeach;?>
 		<div style="width:8%">Checked</div>
 		<div style="width:2%">Published</div>
 	</div>
 	<?php foreach($this->characters as $character):?>
-	<div class="guild-row">
+	<div class="com-guilds-row">
 		<div style="width:4%"><?php echo $character->id;?></div>
 		<div style="width:2%"><input type="checkbox" name="character" value="<?php echo $character->id; ?>"/></div>
 		<div style="width:10%" class="editable"><?php echo $character->name; ?></div>
 		<?php foreach($this->types as $type):?>
 			<?php $type_name = $type->name.'_name'; ?>
 			<?php $type_id = $type->name.'_id'; ?>
-			<div class="guild-<?php echo $type->name;?>">
-				<select>
-					<option value="">-- Select <?php echo ucwords($type->name);?> --</option>
-					<?php foreach($this->categories as $category):?>
-						<?php if($category->type == $type->name):?>
-							<option value="<?php echo $category->id; ?>"<?php if($character->$type_id == $category->id){echo 'selected="selected"';}?> data-parent="<?php echo $category->parent; ?>"><?php echo $category->name;?></option>
-						<?php endif;?>
-					<?php endforeach;?>
-				</select>
-			</div>
+			<select class="com-guilds-<?php echo $type->name;?>" >
+				<option value="">-- Select <?php echo ucwords($type->name);?> --</option>
+				<?php foreach($this->categories as $category):?>
+					<?php if($category->type == $type->name):?>
+						<option value="<?php echo $category->id; ?>"<?php if($character->$type_id == $category->id){echo 'selected="selected"';}?> data-parent="<?php echo $category->parent; ?>"><?php echo $category->name;?></option>
+					<?php endif;?>
+				<?php endforeach;?>
+			</select>
 		<?php endforeach;?>
 		<div class="editable date" style="width:8%"><?php echo $character->checked; ?></div>
 		<div style="width:2%"><?php echo $character->published; ?></div>
