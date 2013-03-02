4c73c7ac8f7228cbc5e9c44c7e371c88ed470aef
Nick Swinford <NicholasJohn16@gmail.com>
2013-03-02 00:52:17 -0500

    Slight reorganization

--- administrator/components/com_guilds/models/members.php
+++ administrator/components/com_guilds/models/members.php
@@ -37,32 +37,32 @@
 		 * Constructor
 		 */
 		function __construct() {
-			parent::__construct();
+                    parent::__construct();
 
-			$option = JRequest::getCmd('option');
-			$mainframe = JFactory::getApplication();
-	 
-			// Get pagination request variables
-			$limit = $mainframe->getUserStateFromRequest('global.list.limit', 'limit', $mainframe->getCfg('list_limit'), 'int');
-			$limitstart = $mainframe->getUserStateFromRequest($option.'limitstart','limitstart',0);
-			
-			// Get filter values for Roster view
-			$order		= $mainframe->getUserStateFromRequest($option."order",'order',null,'cmd' );
-			$direction	= $mainframe->getUserStateFromRequest($option."direction",'direction',null,'word');
-			$search		= $mainframe->getUserStateFromRequest($option."search",'search','','string' );
-			$filter_type= $mainframe->getUserStateFromRequest($option.'filter_type','filter_type',array(),'array');
-			
-			// In case limit has been changed, adjust it
-			//$limitstart = ($limit != 0 ? (floor($limitstart / $limit) * $limit) : 0);
-		 
-			$this->setState('limit', $limit);
-			$this->setState('limitstart', $limitstart);
-			$this->setState('order',$order);
-			$this->setState('direction',$direction);
-			$this->setState('search',$search);
-			$this->setState('filter_type',$filter_type);
-			$this->setState('users',null);
-	    }
+                    $option = JRequest::getCmd('option');
+                    $mainframe = JFactory::getApplication();
+
+                    // Get pagination request variables
+                    $limit = $mainframe->getUserStateFromRequest('global.list.limit', 'limit', $mainframe->getCfg('list_limit'), 'int');
+                    $limitstart = $mainframe->getUserStateFromRequest($option.'limitstart','limitstart',0);
+
+                    // Get filter values for Roster view
+                    $order = $mainframe->getUserStateFromRequest($option."order",'order',null,'cmd' );
+                    $direction = $mainframe->getUserStateFromRequest($option."direction",'direction',null,'word');
+                    $search = $mainframe->getUserStateFromRequest($option."search",'search','','string' );
+                    $filter_type = $mainframe->getUserStateFromRequest($option.'filter_type','filter_type',array(),'array');
+
+                    // In case limit has been changed, adjust it
+                    //$limitstart = ($limit != 0 ? (floor($limitstart / $limit) * $limit) : 0);
+
+                    $this->setState('limit', $limit);
+                    $this->setState('limitstart', $limitstart);
+                    $this->setState('order',$order);
+                    $this->setState('direction',$direction);
+                    $this->setState('search',$search);
+                    $this->setState('filter_type',$filter_type);
+                    $this->setState('users',null);
+                }
     
 	    protected function buildQuery() {
 	    	$select = $this->buildSelect();
@@ -178,128 +178,125 @@
 		    return $this->members;
 		}
 		
-		function getTypeahead() {
-			$query = 'SELECT id,username,"User"
-						FROM jos_users
-						WHERE lower(username) LIKE \'%john%\'
-						UNION
-						SELECT user_id,value,"Handle"
-						FROM jos_community_fields_values
-						WHERE field_id = 29 AND
-						lower(value) LIKE \'%john%\'';
-			
-		}
+        function getTypeahead() {
+            $query = 'SELECT id,username,"User"
+                FROM jos_users
+                WHERE lower(username) LIKE \'%john%\'
+                UNION
+                SELECT user_id,value,"Handle"
+                FROM jos_community_fields_values
+                WHERE field_id = 29 AND
+                lower(value) LIKE \'%john%\'';
+        }
 	
-		function getTotal() {
-		 	// Load the content if it doesn't already exist
-	 		if (empty($this->total)) {
-	 		    $query = $this->buildQuery();
-	 		    $this->total = $this->_getListCount($query);	
-	 		}
-	 		return $this->total;
-	  	}
-  	
-		function getPagination() {
-			// Load the content if it doesn't already exist
-		 	if (empty($this->pagination)) {
-		 		jimport('joomla.html.pagination');
-		     	$this->pagination = new JPagination($this->getTotal(), $this->getState('limitstart'), $this->getState('limit') );
-		 	}
-		 	return $this->pagination;
-	  	}
-  	
-	  	function getCharactersByUserId($user_id) {
-	  		$db =& JFactory::getDBO();
-	  		
-	  		$query = " SELECT * " 
-	  				. " FROM #__char_characters "
-	  				. " WHERE user_id = " . $user_id;
-	  		$db->setQuery($query);
-	  		$result = $db->loadObjectList();
-	  		
-	  		return $result;
-	  	}
-	  	
-	  	function updateStatus() {
-	  		$users = $this->getState('users');
-	  		
-	  	}
-	  	
-	  	function getHandles() {
-	  		$users = $this->getState('users');	
-	  		
-	  		if(empty($this->handles)) {
-	  			$db = JFactory::getDBO();
-	  			$query = " SELECT value "
-	  					." FROM #__community_fields_values "
-	  					." WHERE `field_id` =  29 "
-	  					." AND `user_id` IN (".implode(",",$users).");";
-	  			$db->setQuery($query);
-	  			$this->handles = $this->loadObjectList();
-	  		}
-	  		
-	  		return $this->handles;
-	  	}
-	  	
-	  	function getForumRanks() {
-	  		$users = $this->getState($users);
-
-	  		if(empty($this->forumRanks)) {
-	  			$db = JFactory::getDBO();
-	  			$query = " SELECT `userid`, `rank`, `rank_title` "
-	  					." FROM `jos_kunena_users` AS a "
-	  					." LEFT JOIN `jos_kunena_ranks` AS b ON a.rank = b.rank_id"
-	  					." AND `user_id` IN (".implode(",",$users).");";
-	  			$db->setQuery($query);
-	  			$this->forumRanks = $this->loadObjectList();
-	  		}
-	  		
-	  		return $this->handles;
-	  	
-	  	}
-  	
-    function update($field,$id,$value){
-        $db =& JFactory::getDBO();
-
-        switch($field) {
-            case "forum_rank":
-                $query = " UPDATE " . $db->nameQuote('#__kunena_users')
-                        . " SET rank = " . $db->quote($value) 
-                        . " WHERE userid  = " . $db->quote($id);
-                break;
-            case "appdate":
-                $query = " UPDATE " . $db->nameQuote('#__guilds_members');
-                if($value) {
-                    $query .= " SET appdate = " . $db->quote($value);
-                } else {
-                    $query .= " SET appdate = NULL ";
-                }
-                $query .= " WHERE " . $db->nameQuote('user_id') . " = " . $db->quote($id);
-                break;
-            case "handle";
-                $query = " UPDATE ". $db->nameQuote('#__community_fields_values')
-                    ." SET value = ". $db->quote($value)
-                    ." WHERE ".$db->nameQuote('field_id')." = ". $db->quote('29')
-                    ." AND ".$db->nameQuote('user_id')." = " . $db->quote($id);
-                break;
-            default:
-                JError::raiseError('500','Invalid request',"Name option is incorrect");
-        }
-            dump($query,'Query');
-            $db->setQuery($query);
-            $db->query();
+        function getTotal() {
+            // Load the content if it doesn't already exist
+            if (empty($this->total)) {
+                $query = $this->buildQuery();
+                $this->total = $this->_getListCount($query);	
+            }
+            return $this->total;
         }
   	
-	  	function getRanks() {
-	  		if(empty($this->ranks)) {
-		  		$db =& JFactory::getDBO();
-		  		$query  = " SELECT ".$db->nameQuote('rank_id')." AS value, ".$db->nameQuote('rank_title'). " AS text";
-		  		$query .= " FROM ".$db->nameQuote('#__kunena_ranks');
-		  		$db->setQuery($query);
-		  		$this->ranks = $db->loadObjectList();
-	  		}
-	  		return $this->ranks;
-	  	 }
+        function getPagination() {
+            // Load the content if it doesn't already exist
+            if (empty($this->pagination)) {
+                jimport('joomla.html.pagination');
+                $this->pagination = new JPagination($this->getTotal(), $this->getState('limitstart'), $this->getState('limit') );
+            }
+            return $this->pagination;
+        }
+  	
+        function getCharactersByUserId($user_id) {
+                $db =& JFactory::getDBO();
+
+                $query = " SELECT * " 
+                                . " FROM #__char_characters "
+                                . " WHERE user_id = " . $user_id;
+                $db->setQuery($query);
+                $result = $db->loadObjectList();
+
+                return $result;
+        }
+	  	
+        function updateStatus() {
+                $users = $this->getState('users');
+
+        }
+        
+        function update($field,$id,$value){
+            $db =& JFactory::getDBO();
+
+            switch($field) {
+                case "forum_rank":
+                    $query = " UPDATE " . $db->nameQuote('#__kunena_users')
+                            . " SET rank = " . $db->quote($value) 
+                            . " WHERE userid  = " . $db->quote($id);
+                    break;
+                case "appdate":
+                    $query = " UPDATE " . $db->nameQuote('#__guilds_members');
+                    if($value) {
+                        $query .= " SET appdate = " . $db->quote($value);
+                    } else {
+                        $query .= " SET appdate = NULL ";
+                    }
+                    $query .= " WHERE " . $db->nameQuote('user_id') . " = " . $db->quote($id);
+                    break;
+                case "handle";
+                    $query = " UPDATE ". $db->nameQuote('#__community_fields_values')
+                        ." SET value = ". $db->quote($value)
+                        ." WHERE ".$db->nameQuote('field_id')." = ". $db->quote('29')
+                        ." AND ".$db->nameQuote('user_id')." = " . $db->quote($id);
+                    break;
+                default:
+                    JError::raiseError('500','Invalid request',"Name option is incorrect");
+            }
+                $db->setQuery($query);
+                $db->query();
+        }
+
+        function getHandles() {
+            $users = $this->getState('users');	
+
+            if(empty($this->handles)) {
+                $db = JFactory::getDBO();
+                $query = " SELECT value "
+                        ." FROM #__community_fields_values "
+                        ." WHERE `field_id` =  29 "
+                        ." AND `user_id` IN (".implode(",",$users).");";
+                $db->setQuery($query);
+                $this->handles = $this->loadObjectList();
+            }
+
+            return $this->handles;
+        }
+
+        function getForumRanks() {
+            $users = $this->getState($users);
+
+            if(empty($this->forumRanks)) {
+                $db = JFactory::getDBO();
+                $query = " SELECT `userid`, `rank`, `rank_title` "
+                        ." FROM `jos_kunena_users` AS a "
+                        ." LEFT JOIN `jos_kunena_ranks` AS b ON a.rank = b.rank_id"
+                        ." AND `user_id` IN (".implode(",",$users).");";
+                $db->setQuery($query);
+                $this->forumRanks = $this->loadObjectList();
+            }
+
+            return $this->handles;
+        }
+
+        function getRanks() {
+            if(empty($this->ranks)) {
+                $db =& JFactory::getDBO();
+                $query  = " SELECT ".$db->nameQuote('rank_id')." AS value, ".$db->nameQuote('rank_title'). " AS text";
+                $query .= " FROM ".$db->nameQuote('#__kunena_ranks');
+                $db->setQuery($query);
+                $this->ranks = $db->loadObjectList();
+            }
+            return $this->ranks;
+         }
   	 
 		
 	}
