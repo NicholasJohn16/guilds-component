1196d040b232eff527cbdf383b7a1546763838c1
Nick Swinford <NicholasJohn16@gmail.com>
2013-09-02 15:18:54 -0400

    basic migration script

---
+++ migration.sql
@@ -0,0 +1,16 @@
+# Add usernames to all members
+UPDATE jos_users, jos_guilds_members
+SET jos_guilds_members.username = jos_users.username
+WHERE jos_guilds_members.user_id = jos_users.id
+
+# Update sto_handle to @handles
+UPDATE jos_community_fields_values, jos_guilds_members
+SET jos_guilds_members.sto_handle = jos_community_fields_values.value
+WHERE jos_guilds_members.user_id = jos_community_fields_values.user_id
+AND jos_community_fields_values.field_id = 29
+
+# TOR
+UPDATE jos_community_fields_values, jos_guilds_members
+SET jos_guilds_members.sto_handle = jos_community_fields_values.value
+WHERE jos_guilds_members.user_id = jos_community_fields_values.user_id
+AND jos_community_fields_values.field_id = 39
