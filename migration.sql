db3a0df78322e52492ea79a2b41adb74fb11b911
Nick Swinford <NicholasJohn16@gmail.com>
2013-09-03 02:50:14 -0400

    changes empty rows to NULL

--- migration.sql
+++ migration.sql
@@ -14,3 +14,7 @@ UPDATE jos_community_fields_values, jos_guilds_members
 SET jos_guilds_members.sto_handle = jos_community_fields_values.value
 WHERE jos_guilds_members.user_id = jos_community_fields_values.user_id
 AND jos_community_fields_values.field_id = 39
+
+UPDATE jos_guilds_members SET sto_handle = NULL WHERE sto_handle = "";
+UPDATE jos_guilds_members SET gw2_handle = NULL WHERE gw2_handle = "";
+UPDATE jos_guilds_members SET tor_handle = NULL WHERE tor_handle = "";
