6f804f042b4afb996f5077c6b196149eca7e96da
Nick Swinford <NicholasJohn16@gmail.com>
2013-09-24 20:01:57 -0400

    added alert to display if no characters found

--- components/com_guilds/views/characters/tmpl/roster.php
+++ components/com_guilds/views/characters/tmpl/roster.php
@@ -56,78 +56,85 @@
             <div class="span2 com-guilds-pubdate"><?php $this->sortable("Published"); ?></div>
         </div>
         <?php $i = 0; ?>
-        <?php foreach ($this->characters as $character): ?>
-            <div class="row-fluid">
-                <div class="span1" style="width:30px;">
-                    <input id="cb<?php echo $i ?>" 
-                           type="checkbox" 
-                           value="<?php echo $character->id; ?>" 
-                           name="characters[]"/>
-                </div>
-                <div class="span1">
-                     <?php echo $character->id." | ".$character->user_id; ?>
-                </div>
-                <div class="span1 editable" 
-                     data-name="name" 
-                     data-pk="<?php echo $character->id; ?>" 
-                     data-title="Edit Character Name">
-                         <?php echo $character->name; ?>
-                </div>
-                <?php if($character->handle === NULL) : ?>
-                    <div class="span2 editable"
-                         data-title="Edit Handle"
-                         data-name="<?php echo $game_handle; ?>"
-                         data-pk="<?php echo $character->user_id; ?>"
-                         data-url="index.php?option=com_guilds&view=members&task=ajaxSave&format=raw">
-                             <?php echo $character->$game_handle; ?>
-                    </div>
-                <?php else: ?>
-                    <div class="span2 editable"
-                         data-title="Edit Character Handle"
-                         data-name="handle"
-                         data-pk="<?php echo $character->id;?>"
-                         data-url="index.php?option=com_guilds&view=characters&task=ajaxSave&format=raw">
-                        <?php echo $character->handle; ?>
-                    </div>
-                <?php endif; ?>
-                <div class="span1 status-<?php echo $character->user_id ?>"><?php echo $character->status; ?></div>
-                <?php foreach ($this->types as $type): ?>
-                    <?php $type_name = $type->name . '_name'; ?>
-                    <?php $type_id = $type->name . '_id'; ?>
-                    <div class="span2 editable com-guilds-<?php echo $type->name; ?>" 
-                         data-type="select" 
-                         data-value="<?php echo $character->$type_id; ?>" 
-                         data-title="Edit <?php echo ucfirst($type->name); ?>" 
-                         data-pk="<?php echo $character->id; ?>" 
-                         data-source="index.php?option=com_guilds&view=categories&format=json&type=<?php echo $type->name; ?>" 
-                         data-name="category[<?php echo $type->name; ?>]">
-                             <?php echo $character->$type_name; ?>
-                    </div>
-                <?php endforeach; ?>
-                <div class="span2 editable com-guilds-checked" 
-                     data-type="date" 
-                     data-placement="right" 
-                     data-name="checked" 
-                     data-title="Edit Checked Date" 
-                     data-pk="<?php echo $character->id; ?>">
-                         <?php echo $character->checked; ?>
-                </div>
-                <?php $pub = array('title'=>array('Unpublished','Published'),
-                    'icon'=>array('eye-close icon-white','eye-open'),
-                    'task'=>array('publish','unpublish'),
-                    'class'=>array('btn-inverse','')); ?>
-                <div class="span1 com-guilds-pubdate">
-                    <button title="<?php echo $pub['title'][$character->published] . " Character"; ?>" 
-                            class="btn btn-small publish <?php echo $pub['class'][$character->published]; ?>" 
-                            data-task="<?php echo $pub['task'][$character->published]; ?>" 
-                            data-id="<?php echo $character->id; ?>">
-                        <i class="icon-<?php echo $pub['icon'][$character->published]; ?>"></i>
-                    </button>
-                    <?php echo $character->unpublished_date; ?>
-                </div>
+        <?php if(empty($this->characters)): ?>
+             <div class="alert alert-block alert-info">
+                <h4 class="alert-heading">No Characters Found</h4>
+                <p>Please revise your search and try again.</p>
             </div>
-            <?php $i++; ?>
-        <?php endforeach; ?>
+        <?php else: ?>
+            <?php foreach ($this->characters as $character): ?>
+                <div class="row-fluid">
+                    <div class="span1" style="width:30px;">
+                        <input id="cb<?php echo $i ?>" 
+                               type="checkbox" 
+                               value="<?php echo $character->id; ?>" 
+                               name="characters[]"/>
+                    </div>
+                    <div class="span1">
+                         <?php echo $character->id." | ".$character->user_id; ?>
+                    </div>
+                    <div class="span1 editable" 
+                         data-name="name" 
+                         data-pk="<?php echo $character->id; ?>" 
+                         data-title="Edit Character Name">
+                             <?php echo $character->name; ?>
+                    </div>
+                    <?php if($character->handle === NULL) : ?>
+                        <div class="span2 editable"
+                             data-title="Edit Handle"
+                             data-name="<?php echo $game_handle; ?>"
+                             data-pk="<?php echo $character->user_id; ?>"
+                             data-url="index.php?option=com_guilds&view=members&task=ajaxSave&format=raw">
+                                 <?php echo $character->$game_handle; ?>
+                        </div>
+                    <?php else: ?>
+                        <div class="span2 editable"
+                             data-title="Edit Character Handle"
+                             data-name="handle"
+                             data-pk="<?php echo $character->id;?>"
+                             data-url="index.php?option=com_guilds&view=characters&task=ajaxSave&format=raw">
+                            <?php echo $character->handle; ?>
+                        </div>
+                    <?php endif; ?>
+                    <div class="span1 status-<?php echo $character->user_id ?>"><?php echo $character->status; ?></div>
+                    <?php foreach ($this->types as $type): ?>
+                        <?php $type_name = $type->name . '_name'; ?>
+                        <?php $type_id = $type->name . '_id'; ?>
+                        <div class="span2 editable com-guilds-<?php echo $type->name; ?>" 
+                             data-type="select" 
+                             data-value="<?php echo $character->$type_id; ?>" 
+                             data-title="Edit <?php echo ucfirst($type->name); ?>" 
+                             data-pk="<?php echo $character->id; ?>" 
+                             data-source="index.php?option=com_guilds&view=categories&format=json&type=<?php echo $type->name; ?>" 
+                             data-name="category[<?php echo $type->name; ?>]">
+                                 <?php echo $character->$type_name; ?>
+                        </div>
+                    <?php endforeach; ?>
+                    <div class="span2 editable com-guilds-checked" 
+                         data-type="date" 
+                         data-placement="right" 
+                         data-name="checked" 
+                         data-title="Edit Checked Date" 
+                         data-pk="<?php echo $character->id; ?>">
+                             <?php echo $character->checked; ?>
+                    </div>
+                    <?php $pub = array('title'=>array('Unpublished','Published'),
+                        'icon'=>array('eye-close icon-white','eye-open'),
+                        'task'=>array('publish','unpublish'),
+                        'class'=>array('btn-inverse','')); ?>
+                    <div class="span1 com-guilds-pubdate">
+                        <button title="<?php echo $pub['title'][$character->published] . " Character"; ?>" 
+                                class="btn btn-small publish <?php echo $pub['class'][$character->published]; ?>" 
+                                data-task="<?php echo $pub['task'][$character->published]; ?>" 
+                                data-id="<?php echo $character->id; ?>">
+                            <i class="icon-<?php echo $pub['icon'][$character->published]; ?>"></i>
+                        </button>
+                        <?php echo $character->unpublished_date; ?>
+                    </div>
+                </div>
+                <?php $i++; ?>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </div>
     <input type="hidden" name="task" value=""/>
     <input type="hidden" name="order" value="<?php echo $this->order; ?>"/>
